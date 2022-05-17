41ebe88430950375e4e3f55e596d60710f8db84a
TheWyller <wyller_gt@hotmail.com>
2022-05-17 14:11:41 -0300

    Adcionando modal do Usuario e os redirecionamentos das páginas nos respectivos botões

--- src/script.js
+++ src/script.js
@@ -1,4 +1,40 @@
 import { Dom } from "./models/Dom.js";
+import { User } from "./controllers/User.js";
 
+
+let noLogin = {
+    name: 'Time A',
+    button1:'Login',
+    button2:'Cadastro',
+}
+
+let login = {
+    name: 'Time A',
+    button1:'Dashboard',
+    button2:'Logout',
+}
 
 Dom.createHeader()
+
+if(typeof localStorage.getItem('token') === 'string'){
+    Dom.modalUser(login)
+}else{
+    Dom.modalUser(noLogin)
+}
+
+let modal = document.querySelector('.modalUser')
+
+modal.addEventListener('click',(e)=>{
+    if(e.target.nodeName === 'BUTTON'){
+        if(e.target.innerText === 'Login'){
+            window.location = 'src/pages/login.html'
+        }else if(e.target.innerText === 'Cadastro'){
+            window.location = 'src/pages/cadastro.html'
+        }else if(e.target.innerText === 'Dashboard'){
+            window.location= 'src/pages/dashboard.html'
+        }else{
+            localStorage.clear()
+            window.location.reload()
+        }
+    }
+})
--- src/scriptDash.js
+++ src/scriptDash.js
@@ -1,4 +1,25 @@
 import { Dom } from "./models/Dom.js";
 
 
+let dashboard = {
+    name: 'Time A',
+    button1:'Home',
+    button2:'Logout',
+}
+
 Dom.createHeader()
+Dom.modalUser(dashboard)
+
+
+let modal = document.querySelector('.modalUser')
+
+modal.addEventListener('click',(e)=>{
+    if(e.target.nodeName === 'BUTTON'){
+        if(e.target.innerText === 'Home'){
+            window.location = '../../index.html'
+        }else{
+            localStorage.clear()
+            window.location.reload()
+        }
+    }
+})
